d7d3596b6013548fbda0a9636a3d4f93abc3a080
Pawel Proc <pawel.henryk.proc@gmail.com>
2025-10-02 12:41:59 +0200

    MyService.java

---
+++ src/main/java/com/proccorp/myapp/my/MyService.java
@@ -0,0 +1,10 @@
+package com.proccorp.myapp.my;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+    public void test(){
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/com/proccorp/myapp/my/MyServiceTest.java
@@ -0,0 +1,11 @@
+package com.proccorp.myapp.my;
+
+import org.junit.jupiter.api.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void test(){
+        System.out.println("test2");
+    }
+}
